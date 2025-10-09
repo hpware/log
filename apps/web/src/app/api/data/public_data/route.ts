a3b1ca90c057c5e7e9af893d5006bc8594ae94ea
吳元皓 <hw@yuanhau.com>
2025-10-09 11:38:50 +0800

    this works

--- apps/web/src/app/api/data/public_data/route.ts
+++ apps/web/src/app/api/data/public_data/route.ts
@@ -1,13 +1,26 @@
 import type { NextRequest } from "next/server";
-import { db, dorm } from "@/../../packages/db/src/index";
-import { userPosts } from "@/../../packages/db/src/schema/main";
+import {
+  db,
+  dorm,
+  main_schema,
+  auth_schema,
+} from "../../../../../../../packages/db/src/index";
 
 export const GET = async (request: NextRequest) => {
-  const dbResult = await db
-    .select()
-    .from(userPosts)
-    .where(dorm.eq(userPosts.private, false))
-    .orderBy(dorm.asc(userPosts.createdAt))
-    .limit(50)
-    .offset(0);
+  try {
+    const { searchParams } = new URL(request.url);
+    const offset = searchParams.get("offset") || "0";
+    const dbResult = await db
+      .select()
+      .from(main_schema.userPosts)
+      .where(dorm.eq(main_schema.userPosts.private, false))
+      .orderBy(dorm.asc(main_schema.userPosts.createdAt))
+      .limit(50)
+      .offset(Number(offset));
+    console.log(dbResult);
+    return Response.json({ result: dbResult });
+  } catch (e: any) {
+    console.error(e);
+    return Error("whoops");
+  }
 };
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema1 from "./schema/main";
+import * as main_schema from "./schema/main";
 import * as schema2 from "./schema/auth";
 
 export const db = drizzle(process.env.DATABASE_URL || "", {
-  schema: { ...schema1, ...schema2 },
+  schema: { ...main_schema, ...schema2 },
 });
 
 export * as dorm from "drizzle-orm";
+export * as main_schema from "./schema/main";
+export * as auth_schema from "./schema/auth";
