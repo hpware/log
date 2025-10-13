c97b6f2a9f86405119bcad10c72e726ff6b77141
吳元皓 <hw@yuanhau.com>
2025-10-14 00:25:18 +0800

    this should work?

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -48,6 +48,8 @@ export const metadata: Metadata = {
   description: "The home of ",
 };
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: Readonly<{
