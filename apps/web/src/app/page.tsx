a6986fe59ba7cb9675bbcec6b9a015785af8d0fe
吳元皓 <hw@yuanhau.com>
2025-10-18 23:01:33 +0800

    does this work??

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,35 +1,9 @@
 import { PublicPostsAndVideos } from "@/components/publicPostsAndVideos";
 import type { Metadata } from "next";
-import { db, main_schema, dorm } from "../../../../packages/db/src";
 
-export async function generateMetadata(): Promise<Metadata> {
-  try {
-    const titleResult = await db
-      .select()
-      .from(main_schema.kvData)
-      .where(dorm.eq(main_schema.kvData.key, "title"));
-
-    const descResult = await db
-      .select()
-      .from(main_schema.kvData)
-      .where(dorm.eq(main_schema.kvData.key, "description"));
-
-    const title = titleResult[0]?.value ?? "";
-    const description = `${descResult[0]?.value ?? ""}`;
-
-    return {
-      title: `Home | ${title}`,
-      description,
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      title: `Home`,
-    };
-  }
-}
-
-export const dynamic = "auto";
+export const metadata: Metadata = {
+  title: `Home`,
+};
 
 export default async function Home() {
   return (
