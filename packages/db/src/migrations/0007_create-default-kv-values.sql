f4eca096c1b8faf5cef696bfb5091f90ac6ad0a1
吳元皓 <hw@yuanhau.com>
2025-10-19 11:57:52 +0800

    This should fix in prod now

--- packages/db/src/migrations/0007_create-default-kv-values.sql
+++ packages/db/src/migrations/0007_create-default-kv-values.sql
@@ -1,5 +1,5 @@
 INSERT INTO kv_data (key, value) VALUES
-('title', ''),
-('description', 'Welcome to your instence of hpware/log!'),
-('owner', 'Server owner')
+('title', '"Title here"'),
+('description', '"Welcome to your instance of hpware/log!"'),
+('owner', '"Server owner"')
 ON CONFLICT DO NOTHING;
