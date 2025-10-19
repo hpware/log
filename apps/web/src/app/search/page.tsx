1c1bf80d0349df510a3966e429f6dee2db1af0c4
吳元皓 <hw@yuanhau.com>
2025-10-19 11:32:33 +0800

    ???

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,13 +1,32 @@
-import { useEffect, useState } from "react";
+"use client";
+import { Suspense, useEffect, useState } from "react";
 import type { Metadata } from "next";
 import { useRouter, useSearchParams } from "next/navigation";
 import { SearchIcon } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import { Spinner } from "@/components/ui/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
-export const dynamic = "auto";
 
 export default function Page() {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex flex-col justify-center absolute inset-0 transition-all duration-300 ease-in-out">
+          <SearchIcon className="justifty-center mx-auto w-12 h-12 transition-all duration-300 ease-in-out" />
+          <span className="text-center justify-center text-2xl font-bold geint-sans transition-all duration-300 ease-in-out mb-4">
+            Search anything!
+          </span>
+          <Skeleton className="border rounded-xl w-[70%] max-w-[500px] justify-center mx-auto px-2 py-1 resize-none overflow-y-hidden overflow-scroll overflow-x-hidden whitespace-nowrap"></Skeleton>
+        </div>
+      }
+    >
+      <SearchFunction />
+    </Suspense>
+  );
+}
+
+function SearchFunction() {
   const [searchBox, setSearchBox] = useState("");
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -20,6 +39,11 @@ export default function Page() {
   }, []);
   const updateSearchBox = (value: string) => {
     setSearchBox(value);
+    if (value.length !== 0) {
+      router.push(`/search?query=${value.replaceAll(" ", "+")}`);
+    } else {
+      router.push("/search");
+    }
   };
   const { data, error, isPending, status } = useQuery({
     queryKey: ["searchData", String(searchBox)],
@@ -45,10 +69,15 @@ export default function Page() {
           Search anything!
         </span>
         <textarea
-          className="border rounded-xl w-[70%] max-w-[500px] justify-center mx-auto px-2 py-1 resize-none"
+          className="border rounded-xl w-[70%] max-w-[500px] justify-center mx-auto px-2 py-1 resize-none overflow-y-hidden overflow-scroll overflow-x-hidden whitespace-nowrap"
           rows={1}
           value={searchBox}
           onChange={(e) => updateSearchBox(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}
           placeholder="Type anything..."
         ></textarea>
       </div>
